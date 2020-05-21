e84502080e06932456bee13473552dfbb9ddbff3
YARI GUADALUPE TOLEDO HERRERA <yaritoledoherrera@hotmail.com>
2020-05-21 18:21:27 -0500

    Curso_Tema

---
+++ Controllers/Curso_TemaController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLaboratorio.Controllers
+{
+    public class Curso_TemaController : Controller
+    {
+        //
+        // GET: /Curso_Tema/
+
+        RepositorioCurso_Tema repoCurso_Tema = new RepositorioCurso_Tema();
+
+        public ActionResult Index()
+        {
+            return View(repoCurso_Tema.obtenerCurso_Temas());
+        }
+
+        public ActionResult Curso_TemaDelete(int id)
+        {
+
+            return View(repoCurso_Tema.obtenerCurso_Tema(id));
+        }
+
+        [HttpPost]
+        public ActionResult Curso_TemaDelete(int id, FormCollection datos)
+        {
+            repoCurso_Tema.eliminarCurso_Tema(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Cursp_TemaDetails(int id)
+        {
+            return View(repoCurso_Tema.obtenerCurso_Tema(id));
+
+        }
+
+        public ActionResult Curso_TemaEdit(int id)
+        {
+            return View(repoCurso_Tema.obtenerCurso_Tema(id));
+
+        }
+
+        [HttpPost]
+        public ActionResult Curso_TemaEdit(int id, Curso_Tema datosCurso_Tema)
+        {
+            datosCurso_Tema.IdCT = id;
+            repoCurso_Tema.actualizarCurso_Tema(datosCurso_Tema);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Curso_TemaCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Curso_TemaCreate(Curso_Tema datos)
+        {
+            repoTema.insertarCurso_Tema(datos);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Curso_Tema.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLaboratorio.Models
+{
+    public class Empleado
+    {
+        public int IdCT { get; set; }
+        public string idCurso { get; set; }
+        public string idTema { get; set; }
+    }
+}
